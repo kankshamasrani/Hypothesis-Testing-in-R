bc3ff543e80bb350d76624fd66e76f664a763e10
Sujith Vallepalli <sujithkumarvallepalli@gmail.com>
2017-02-05 16:17:36 -0500

    Update hypothesis testing.r

--- hypothesis testing.r
+++ hypothesis testing.r
@@ -127,4 +127,64 @@ round(p1,4)
 #Since p value is 0, we reject the hypothesis that 75th percentile of value of age is 7.7 
 
 #################################################################################
+#########################################################################
+
+#Now let us have a look at another real time problem (with two sample test):
+
+#A customer complained to the owner (of an independent fast-food restaurant) that the
+#restaurant is discriminating against the elderly. 
+#The customer claims that 60 years old and older are given fewer french fries
+#than people under 60. 
+
+###Step 1: Data Collection (Random Sampling)
+#The owner responds by gathering data, collected without the 
+#knowledge of the employees so as not to affect their behavior.
+#Here are data on the weight of french fries (grams) for the two groups of customers:
+#Age less than 60: 75 77 80 69 73 76 78 74 75 81 75 80 79 80
+#Age greater than 60: 68 74 77 71 73 75 80 77 78 72 69 71 75 78
+
+
+#Let us conduct a nonparametric two sample test to evaluate the complaint.
+
+
+
+##Loading data into vectors
+young=c(75,77,80,69,73,76,78,74,75,81,75,80,79,80)
+old=c(68,74,77,71,73,75,80,77,78,72,69,71,75,78)
+
+#calculating difference in mean ages of young and old
+tstat2=mean(young)-mean(old)
+tstat2
+
+#this function samples the young and old age data together and selects two groups. And finds the difference between 
+#those two groups.
+
+f3=function(){
+  x=c(young,old)
+  y=sample(x) #juggling the data
+  return(mean(y[1:14])-mean(y[15:28]))
+}
+
+#creating a distribution of sample means and plotting its density plot
+sdist2=replicate(10000,f3())
+plot(density(sdist2))
+
+#Calculating difference or distance of calculated mean and mean from samples
+gap2=abs(mean(sdist2)-tstat2)
+a2=mean(sdist2)-gap2####MEAN ONLY
+b2=mean(sdist2)+gap2####MEAN ONLY
+abline(v=a2,col="red")
+abline(v=b2,col="red")
+
+#calculating p value or the probability of the claim that there is discrimination among age groups
+s12=sdist[sdist2<a2 |sdist2>b2]
+p2=length(s12)/length(sdist2)
+round(p2,4)
+
+#Since the p value is greater than 0.05(assumed significance value),
+#we can say that there is enough evidence that claim of age discrimination may be true.
+#Therefore we fail to reject customers hypothesis that "discrimination shown by employees in restaurent among the
+#customer age groups"
+
+#####################################################################################################
 #End of Script!#
